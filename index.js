270c31983e562623e30fb79e48d5d185080907a5
Joshitha Ganagalla <166937036+GanagallaJoshitha@users.noreply.github.com>
2025-11-14 21:08:31 +0530

    Refactor isPrime function for better validation

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 function isPrime(num) {
-    if (num <= 1) return false;
+    if (!Number.isInteger(num) || num <= 1) return false;
 
-    for (let i = 2; i <= Math.sqrt(num); i++) {
+    const sqrtNum = Math.sqrt(num);
+    for (let i = 2; i <= sqrtNum; i++) {
         if (num % i === 0) return false;
     }
-
     return true;
 }
-
-console.log(isPrime(7)); 
-function evenOdd(num) {
-    return num % 2 === 0;
-}
-
-console.log(evenOdd(10));
